46cb6259acb64f1b0f935952cfb96ae09e468459
Hugo Sabourin <hsabouri@student.42.fr>
2017-12-22 23:50:40 +0100

    colleen corrected

--- C/Colleen.c
+++ C/Colleen.c
@@ -2,10 +2,13 @@
 /*
 	Comment
 */
-char *s="#include <stdio.h>%c/*%c%cComment%c*/%cchar *s=%c%s%c;%cint main() {%c%c/*%c%c%cComment%c%c*/%c%cprintf(s, 10, 10, 9, 10, 10, 34, s, 34, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10);%c}%c";
+void function() {
+	char *s="#include <stdio.h>%c/*%c%cComment%c*/%cvoid function() {%c%cchar *s=%c%s%c;%c%cprintf(s, 10, 10, 9, 10, 10, 10, 9, 34, s, 34, 10, 9, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10);%c}%cint main() {%c%c/*%c%c%cComment%c%c*/%c%cfunction();%c}%c";
+	printf(s, 10, 10, 9, 10, 10, 10, 9, 34, s, 34, 10, 9, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10);
+}
 int main() {
 	/*
 		Comment
 	*/
-	printf(s, 10, 10, 9, 10, 10, 34, s, 34, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 10);
+	function();
 }
